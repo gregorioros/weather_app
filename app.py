23c5656b7acc14d65d2dcd82cb07141754621b0d
gregorioros <119988674+gregorioros@users.noreply.github.com>
2023-11-08 20:03:53 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv,dotenv_values
+
+config = dotenv_values ('.env')
+
+app = Flask(__name__)
+
+def get_weather_data(city):
+   API_KEY = config['API_KEY']
+   url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
+   r = requests.get(url).json()
+   print (r)
+   return r
+
+@app.route("/grego")
+def grego ():
+   get_weather_data('Guayaquil')
+   return get_weather_data("Guayaquil")
+    
+@app.route("/clima")
+def clima ():
+   return "Clima"
+
+if __name__ == "__main__":
+    app.run(debug=True)
